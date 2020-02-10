76f471d7856ccdfacaa105e637d966fe786e3cc6
AnnieHwang <ah345364@gmail.com>
2020-02-10 15:31:08 +0900

    ux change

--- discovery-prep-frontend/src/app/prepbot/prep/prep-list.component.ts
+++ discovery-prep-frontend/src/app/prepbot/prep/prep-list.component.ts
@@ -11,12 +11,31 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+// import { Component, ElementRef, Injector } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
+// import * as _ from 'lodash';
+// import { AbstractComponent } from '../../common/component/abstract.component';
+// import {DataflowService} from '../dataflow/service/dataflow.service';
+// import {isNullOrUndefined} from "util";
+// import {Alert} from "../../common/util/alert.util";
+// import {PrDataflow} from "../../domain/data-preparation/pr-dataflow";
+// import {StringUtil} from "../../common/util/string.util";
 
-import { Component, ElementRef, Injector } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import * as _ from 'lodash';
-import { AbstractComponent } from '../../common/component/abstract.component';
+import {Component, ElementRef, HostListener, Injector, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AbstractComponent} from '../../common/component/abstract.component';
 import {DataflowService} from '../dataflow/service/dataflow.service';
+import {PrDataflow} from '../../domain/data-preparation/pr-dataflow';
+import {Modal} from '../../common/domain/modal';
+import {DeleteModalComponent} from '../../common/component/modal/delete/delete.component';
+import {Alert} from '../../common/util/alert.util';
+import {MomentDatePipe} from '../../common/pipe/moment.date.pipe';
+import {isNullOrUndefined} from "util";
+import {StringUtil} from "../../common/util/string.util";
+import {ActivatedRoute} from "@angular/router";
+import * as _ from 'lodash';
+
+
+const DEFAULT_VIEW_TYPE = 'CARD';
 
 @Component({
   selector: 'prep-list',
@@ -27,6 +46,8 @@ export class PrepListComponent extends AbstractComponent {
   /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
    | Private Variables
    |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+  // 검색 파라메터
+  private _searchParams: { [key: string]: string };
 
   /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
    | Protected Variables
@@ -38,6 +59,17 @@ export class PrepListComponent extends AbstractComponent {
 
   // 프로필 기본 이미지 경로
   public defaultProfileImageSrc = '../../assets/images/img_photo.png';
+
+  public dataflows : PrDataflow[];
+
+  // search text
+  public searchText: string;
+
+  // 뷰타입 LIST, CARD
+  public viewMode = DEFAULT_VIEW_TYPE;
+
+  // 정렬
+  public selectedContentSort: Order = new Order();
 
   /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
    | Constructor
@@ -97,12 +129,159 @@ export class PrepListComponent extends AbstractComponent {
           .then();
   }
 
-  /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-   | Protected Method
-   |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+  public toggleViewMode(viewMode){
+      this.viewMode = viewMode;
+      if ('grid' === this.viewMode) {
+          // this.updateGrid(this.rightDataset.gridData, this.rightGrid).then(() => {
+          //     this.rightSelCols.forEach(colName => this.rightGrid.columnSelection(colName));
+          // });
+      }
+  }
 
-  /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
-   | Private Method
-   |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+    /**
+     * 페이지를 새로 불러온다.
+     * @param {boolean} isFirstPage
+     */
+    public reloadPage(isFirstPage: boolean = true) {
+        (isFirstPage) && (this.page.page = 0);
+        this._searchParams = this._getDfParams();
+        this.router.navigate(
+            [this.router.url.replace(/\?.*/gi, '')],
+            {queryParams: this._searchParams, replaceUrl: true}
+        ).then();
+    } // function - reloadPage
 
+
+
+    /**
+   * Fetch dataflow list
+   */
+  public getDataflows() {
+
+      this.loadingShow();
+
+      this.dataflows = [];
+
+      const params = this._getDfParams();
+
+      this.dataflowService.getDataflowList(params).then((data) => {
+
+          // 현재 페이지에 아이템이 없다면 전 페이지를 불러온다
+          let nullOrUndefined = isNullOrUndefined(data['_embedded']);
+          let preparationdatasets = data['_embedded'].preparationdatasets;
+
+          if (this.page.page > 0 &&
+              (nullOrUndefined || (!nullOrUndefined && preparationdatasets && preparationdatasets.length === 0)))
+          {
+              this.page.page = data['page'].number - 1;
+              this.getDataflows();
+          }
+
+          this._searchParams = params;
+
+          this.pageResult = data['page'];
+
+          this.dataflows = data['_embedded']? this.dataflows.concat(data['_embedded']['preparationdataflows']) : [];
+
+          this.loadingHide();
+
+      }).catch((error) => {
+
+          this.loadingHide();
+
+          if(error.status && error.status===500) {
+              Alert.error(error.message);
+          } else {
+              Alert.warning(error.message);
+          }
+
+      });
+  }
+
+    /**
+     * Search dataflow
+     * @param event
+     */
+    public searchDataflows(event) {
+
+        if (13 === event.keyCode || 27 === event.keyCode) {
+            if (event.keyCode === 27) {
+                this.searchText = '';
+            }
+            this.reloadPage();
+        }
+    }
+
+
+    /**
+   * Change order of list
+   * @param key
+   */
+  public changeOrder(key: string) {
+
+      /// 초기화
+      this.selectedContentSort.sort = this.selectedContentSort.key !== key ? 'default' : this.selectedContentSort.sort;
+      // 정렬 정보 저장
+      this.selectedContentSort.key = key;
+
+      if (this.selectedContentSort.key === key) {
+          // asc, desc
+          switch (this.selectedContentSort.sort) {
+              case 'asc':
+                  this.selectedContentSort.sort = 'desc';
+                  break;
+              case 'desc':
+                  this.selectedContentSort.sort = 'asc';
+                  break;
+              case 'default':
+                  this.selectedContentSort.sort = 'desc';
+                  break;
+          }
+      }
+      /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+       | Protected Method
+       |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+
+      /*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+       | Private Method
+       |-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*/
+
+
+    /**
+     * Returns parameter for dataflow list
+     * @private
+     */
+    private _getDfParams(): any {
+        const params = {
+            page: this.page.page,
+            size: this.page.size,
+            projection: 'forListView',
+            pseudoParam : (new Date()).getTime()
+        };
+
+        if (!isNullOrUndefined(this.searchText) || StringUtil.isNotEmpty(this.searchText)) {
+            params['dfName'] = this.searchText;
+        }
+
+        this.selectedContentSort.sort !== 'default' && (params['sort'] = this.selectedContentSort.key + ',' + this.selectedContentSort.sort);
+
+        return params;
+    }
+
+    /**
+     * Initialise values
+     * @private
+     */
+    private _initView() {
+        this.dataflows = [];
+        this.searchText = '';
+        this.selectedContentSort.sort = 'desc';
+        this.selectedContentSort.key = 'createdTime';
+    }
+
+}
+
+class Order {
+    key: string = 'createdTime';
+    sort: string = 'default';
 }
