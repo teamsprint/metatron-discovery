1912b93e8b634e1a0d661ae9270713a44a788145
exntu.metatron <saaks@exntu.com>
2020-06-05 14:14:32 +0900

    recipe repository add findByNameContaining

--- discovery-server/src/main/java/app/metatron/discovery/domain/dataprep/repository/RecipeRepository.java
+++ discovery-server/src/main/java/app/metatron/discovery/domain/dataprep/repository/RecipeRepository.java
@@ -1,13 +1,19 @@
 package app.metatron.discovery.domain.dataprep.repository;
 
 
+import app.metatron.discovery.domain.dataprep.entity.Dataset;
 import app.metatron.discovery.domain.dataprep.entity.Recipe;
 import app.metatron.discovery.domain.dataprep.entity.RecipeProjections;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "recipes", itemResourceRel = "recipe", collectionResourceRel = "recipe"
         , excerptProjection = RecipeProjections.DefaultProjection.class)
 public interface RecipeRepository extends JpaRepository<Recipe, String> {
+
+    Page<Recipe> findByNameContaining(@Param("name") String name, Pageable pageable);
 
 }
