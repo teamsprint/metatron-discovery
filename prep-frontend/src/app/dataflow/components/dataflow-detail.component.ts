06efba4ed613148f6fa1b810a4756fce08d0c1f8
Song, Hee-Youn <koreamanr@exntu.com>
2020-06-11 14:12:11 +0900

    Fixed script error

--- prep-frontend/src/app/dataflow/components/dataflow-detail.component.ts
+++ prep-frontend/src/app/dataflow/components/dataflow-detail.component.ts
@@ -10,4 +10,5 @@ export class DataflowDetailComponent {
 
   public readonly ROUTER_URLS = RouterUrls;
   public readonly COMMON_UTIL = CommonUtil;
+  public readonly UUID = this.COMMON_UTIL.Generate.makeUUID();
 }
