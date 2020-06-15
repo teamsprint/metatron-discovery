9c6656627c4cf988e8dd7b2882aad206fc7eeacd
Song, Hee-Youn <koreamanr@exntu.com>
2020-06-15 19:48:18 +0900

    Modify feature names for local storage services

--- prep-frontend/src/app/common/services/local-storage/main-local-storage/main-local-storage.service.ts
+++ prep-frontend/src/app/common/services/local-storage/main-local-storage/main-local-storage.service.ts
@@ -10,15 +10,15 @@ export class MainLocalStorageService {
   private readonly VIEW_MODE = ViewMode;
   private readonly DEFAULT_VIEW_MODE = this.VIEW_MODE.CARD;
 
-  saveMainViewMode(viewMode: ViewMode) {
+  saveViewMode(viewMode: ViewMode) {
     localStorage.setItem(LocalStorageConstant.KEY.MAIN_VIEW_MODE, viewMode);
   }
 
-  currentMainViewMode() {
+  currentViewMode() {
     return localStorage.getItem(LocalStorageConstant.KEY.MAIN_VIEW_MODE);
   }
 
-  setDefaultMainViewMode() {
+  setDefaultViewMode() {
     localStorage.setItem(LocalStorageConstant.KEY.MAIN_VIEW_MODE, this.DEFAULT_VIEW_MODE);
   }
 }
