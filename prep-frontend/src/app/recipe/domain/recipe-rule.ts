538a362bd8bcfced7bab45f879533f4939d4883d
Song, Hee-Youn <koreamanr@exntu.com>
2020-07-01 14:13:20 +0900

    Modify recipe rule domain

--- prep-frontend/src/app/recipe/domain/recipe-rule.ts
+++ prep-frontend/src/app/recipe/domain/recipe-rule.ts
@@ -1,31 +1,56 @@
 export class RecipeRule {
 
+  static readonly CREATE = { command: 'create', alias: 'Cr' };
+  static readonly HEADER = { command: 'header', alias: 'He' };
+  static readonly KEEP = { command: 'keep', alias: 'Ke' };
+  static readonly REPLACE = { command: 'replace', alias: 'Rp' };
+  static readonly RENAME = { command: 'rename', alias: 'Rm' };
+  static readonly SET = { command: 'set', alias: 'Se' };
+  static readonly SETTYPE = { command: 'settype', alias: 'St' };
+  static readonly COUNTPATTERN = { command: 'countpattern', alias: 'Co' };
+  static readonly SPLIT = { command: 'split', alias: 'Sp' };
+  static readonly DERIVE = { command: 'derive', alias: 'Dr' };
+  static readonly DELETE = { command: 'delete', alias: 'De' };
+  static readonly DROP = { command: 'drop', alias: 'Dp' };
+  static readonly PIVOT = { command: 'pivot', alias: 'Pv' };
+  static readonly UNPIVOT = { command: 'unpivot', alias: 'Up' };
+  static readonly JOIN = { command: 'join', alias: 'Jo' };
+  static readonly EXTRACT = { command: 'extract', alias: 'Ex' };
+  static readonly FLATTEN = { command: 'flatten', alias: 'Fl' };
+  static readonly MERGE = { command: 'merge', alias: 'Me' };
+  static readonly NEST = { command: 'nest', alias: 'Ne' };
+  static readonly UNNEST = { command: 'unnest', alias: 'Un' };
+  static readonly AGGREGATE = { command: 'aggregate', alias: 'Ag' };
+  static readonly SORT = { command: 'sort', alias: 'So' };
+  static readonly MOVE = { command: 'move', alias: 'Mv' };
+  static readonly UNION = { command: 'union', alias: 'Ui' };
+
   public static of() {
     return [
-      { command: 'create', alias: 'Cr' },
-      { command: 'header', alias: 'He' },
-      { command: 'keep', alias: 'Ke' },
-      { command: 'replace', alias: 'Rp' },
-      { command: 'rename', alias: 'Rm' },
-      { command: 'set', alias: 'Se' },
-      { command: 'settype', alias: 'St' },
-      { command: 'countpattern', alias: 'Co' },
-      { command: 'split', alias: 'Sp' },
-      { command: 'derive', alias: 'Dr' },
-      { command: 'delete', alias: 'De' },
-      { command: 'drop', alias: 'Dp' },
-      { command: 'pivot', alias: 'Pv' },
-      { command: 'unpivot', alias: 'Up' },
-      { command: 'join', alias: 'Jo' },
-      { command: 'extract', alias: 'Ex' },
-      { command: 'flatten', alias: 'Fl' },
-      { command: 'merge', alias: 'Me' },
-      { command: 'nest', alias: 'Ne' },
-      { command: 'unnest', alias: 'Un' },
-      { command: 'aggregate', alias: 'Ag' },
-      { command: 'sort', alias: 'So' },
-      { command: 'move', alias: 'Mv' },
-      { command: 'union', alias: 'Ui' }
+      RecipeRule.CREATE,
+      RecipeRule.HEADER,
+      RecipeRule.KEEP,
+      RecipeRule.REPLACE,
+      RecipeRule.RENAME,
+      RecipeRule.SET,
+      RecipeRule.SETTYPE,
+      RecipeRule.COUNTPATTERN,
+      RecipeRule.SPLIT,
+      RecipeRule.DERIVE,
+      RecipeRule.DELETE,
+      RecipeRule.DROP,
+      RecipeRule.PIVOT,
+      RecipeRule.UNPIVOT,
+      RecipeRule.JOIN,
+      RecipeRule.EXTRACT,
+      RecipeRule.FLATTEN,
+      RecipeRule.MERGE,
+      RecipeRule.NEST,
+      RecipeRule.UNNEST,
+      RecipeRule.AGGREGATE,
+      RecipeRule.SORT,
+      RecipeRule.MOVE,
+      RecipeRule.UNION
     ];
   }
 
